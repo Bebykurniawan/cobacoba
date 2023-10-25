a874dcc7505544ce4683574650a8425d9b8793dd
Beby Kurniawan <bebykurniawan2006@gmail.com>
2023-10-25 19:46:08 +0700

    add file html lalalala

---
+++ index.js
@@ -0,0 +1,10 @@
+const judul = document.getElementById('judul')
+judul.style.color = 'red'
+
+const paragraf = document.getElementsByTagName('p')[0]
+paragraf.style.color = 'red' 
+
+const p4 = document.querySelector('#b p ')
+p4.style.backgroundColor = 'red'
+const p = document.querySelector('p ')
+p.style.backgroundColor = 'blue'
